633a902b59aee780b35debd1c08d153112c87d12
Paweł Ciepliński <pawelcieplinski94@gmail.com>
2019-01-07 23:00:35 +0100

    Add part of static data launching

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        FlowShopScheduling fs = new FlowShopScheduling(5, 2, 4, 100,
-                20, 1.0, 0.3, Crossover.PMXCROSSOVER);
-        fs.schedule();
+        FlowShopScheduling fs = new FlowShopScheduling(4, 2, 4, 3,
+                0, 1.0, 0.3, Crossover.PMXCROSSOVER);
+
+//        fs.scheduleWithRandom();
+        fs.scheduleWithData();
     }
 }
--- src/Population.java
+++ src/Population.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
