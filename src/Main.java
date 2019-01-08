a899f8cff4a9422361d144f5a0974d7b1aa19665
Paweł Ciepliński <pawelcieplinski94@gmail.com>
2019-01-08 21:25:21 +0100

    Finish static data launching

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         FlowShopScheduling fs = new FlowShopScheduling(4, 2, 4, 3,
-                0, 1.0, 0.3, Crossover.PMXCROSSOVER);
+                0, 1.0, 0.1, Crossover.PMXCROSSOVER);
 
 //        fs.scheduleWithRandom();
         fs.scheduleWithData();
